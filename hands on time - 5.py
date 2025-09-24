11edee459a2583087f2af7d6be127075b0e3cc3d
C. S. Evelyin Vishali <csevelyinvishali@gmail.com>
2025-09-24 06:32:02 +0530

    Added 5 python exercises
    This commit includes basic python programs like accessing list, join formatting and other beginner exercises

---
+++ hands on time - 5.py
@@ -0,0 +1,14 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> play_list = ["Track A","Track B","Track C","Track D"]
+>>> play_list.remove("Track C")
+>>> play_list.insert(0,"Track X")
+>>> play_list.remove("Track D")
+>>> play_list.insert(1,"Track D")
+>>> print(" ".join(playlist))
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    print(" ".join(playlist))
+NameError: name 'playlist' is not defined. Did you mean: 'play_list'?
+>>> print(" ".join(play_list))
+Track X Track D Track A Track B
